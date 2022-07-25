60032053a6acb2c9539bdfe98d43a967c16b9319
winryesforty <winryesforty@gmail.com>
2022-07-25 11:04:22 +0800

    copy pasted project ci3_newfile to ci3_practice and created github repo and cloned

---
+++ application/controllers/Usercontroller.php
@@ -0,0 +1,117 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Usercontroller extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('User_model');
+		
+	}
+	public function index()
+	{
+		$data['users'] = $this->User_model->getAllUser();
+		$this->load->view('template/header');
+		$this->load->view('dashboard/index', $data);
+		$this->load->view('template/footer');
+	}
+	public function dashboard2()
+	{
+		$data = "Hey";
+		$this->load->view('template/header');
+		$this->load->view('dashboard/dashboard2');
+		$this->load->view('template/footer');
+
+		// force_download("dashboard2", $data);
+	}
+	public function dashboard3()
+	{
+		$this->load->view('template/header');
+		$this->load->view('dashboard/dashboard3');
+		$this->load->view('template/footer');
+	}
+	public function dashboard4()
+	{
+		$this->load->view('template/header');
+		$this->load->view('dashboard/dashboard4');
+		$this->load->view('template/footer');
+	}
+
+	public function dashboard5()
+	{
+		$data['content'] = '';
+		$this->load->view('template/header');
+		$this->load->view('dashboard/dashboard5');
+		$this->load->view('template/footer');
+	}
+
+	public function updateuser()
+	{
+		// $query = $this->User_model->update_user();//
+		if($this->User_model->update_user())
+		{
+			$this->session->set_flashdata('message', 'User successfully updated.');
+			redirect('');
+		}
+	}
+	
+	public function user_delete($id=NULL)
+	{
+		if($this->User_model->delete_user($id))
+		{
+			$this->session->set_flashdata('message', "User Successfully Deleted");
+			redirect('');
+		}
+	}
+	public function newUser()
+	{
+		$config['upload_path']          = './assets/images/';
+		$config['allowed_types']        = 'gif|jpg|png|jfif|jpeg';
+		$config['max_size']             = 19000;
+		$config['max_width']            = 5024;
+		$config['max_height']           = 5768;
+		$config['file_name']			= $this->input->post('last_name').$this->input->post('first_name');
+
+		$this->upload->initialize($config);
+		
+		if ( ! $this->upload->do_upload('user_image') )
+		{
+				$error = array('error' => $this->upload->display_errors());
+
+				echo $error['error']. "-".$config['upload_path'];
+				exit;
+				$this->load->view('upload_form', $error);
+		}
+		else
+		{
+			$imageUploaded = $this->upload->data();
+			$array_data = array(
+				'first_name'	=>	$this->input->post('first_name'), 
+				'middle_name'	=>	$this->input->post('middle_name'),
+				'last_name'		=>	$this->input->post('last_name'),
+				'street_address'=>	$this->input->post('street_address'),
+				'city'			=>	$this->input->post('city'),
+				'zip_code'		=>	$this->input->post('zip_code'),
+				'province'		=>	$this->input->post('province'),
+				'image_name'	=>	$imageUploaded['file_name'],
+				'image_path'	=>	$config['upload_path']
+			);
+			
+				$query = $this->db->insert('user', $array_data);
+				if($query)
+				{
+					$data = array(
+						'upload_data'	=> $this->upload->data(),
+						'message'		=> "User successfully saved."
+					);
+					$this->session->set_flashdata('message', 'User successfully saved.');
+					redirect('dashboard2');
+				}
+		}
+		
+	}
+
+
+
+}
---
+++ application/views/dashboard/dashboard2.php
@@ -0,0 +1,236 @@
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">Add User</h1>
+      <!-- <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-group me-2">
+          <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+        </div>
+        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+          <span data-feather="calendar"></span>
+          This week
+        </button>
+      </div> -->
+    </div>
+
+    <!-- <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas> -->
+
+    <div class="table-responsive">
+        
+    <h4>User Info</h4>
+    <?php if($this->session->flashdata('message')){ ?>
+              <p class="container alert alert-success"> <?php echo $this->session->flashdata('message'); ?></p>
+    <?php } ?>
+        <form class="needs-validation" novalidate method="POST" action="usersubmit" enctype="multipart/form-data">
+            <!-- <div class="container col-sm-3 w-10 h-10">
+                <div class="w-100 h-100 border border-primary">
+                    <img id="frame" src="<?php echo base_url('assets/images/default.jpg'); ?>" class="img-fluid w-100 h-100"/>
+                </div>
+                <div class="w-100 h-100 mb-5 border border-solid border-gray">
+                    <input class="form-control" type="file" name="user_image" id="formFile" onchange="preview()">
+                </div>
+            </div> -->
+
+            <script>
+                function preview() {
+                    frame.src = URL.createObjectURL(event.target.files[0]);
+                }
+                function clearImage() {
+                    document.getElementById('formFile').value = null;
+                    frame.src = "";
+                }
+            </script>
+            <div class="modal-body add-modal-body">
+                <div class="row d-flex flex-row-reverse">
+                    <div class="col-md-4 justify-content-center">
+                        <!-- 
+                        <div class="col-md-10 text-center flex-fill">
+                            <label for="validationCustom01">Profile Picture</label>
+                            <img id="frame" src="<?php echo base_url('assets/images/').$user->image_name; ?>" class="frame img-fluid w-100 h-100"/>
+                            <input class="formFile form-control" type="file" name="user_image" class="formFile">
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                        </div> 
+                        -->
+                        <div class="col-md-10 text-center flex-fill">
+                            <label for="validationCustom01">Profile Picture</label>
+                            <img id="frame" src="<?php echo base_url('assets/images/default.jpg'); ?>" class="img-fluid"/>
+                            <input class="form-control formFile" type="file" name="user_image" id="formFile" onchange="preview()" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                    <div class="form-row">
+                        <div class="col-md-12 mb-3">
+                            <label>First name</label>
+                            <div class="col-md-12 mb-3">
+                            <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="" name="first_name" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-12 mb-3">
+                            <label>Middle name</label>
+                            <div class="col-md-12 mb-3">
+                            <input type="text" class="form-control" id="validationCustom01" placeholder="Middle name" value="" name="middle_name" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-12 mb-3">
+                            <label for="validationCustom02">Last name</label>
+                            <div class="col-md-12 mb-3">
+                                <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="" name="last_name" required>
+                                <div class="valid-feedback">
+                                    Looks good!
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="form-row col-md-12 mb-3">
+                    <label>Street Address</label>
+                    <div class="col-md-12 mb-3">
+                        <input type="text" value='' name="street_address" class="form-control" id="inputAddress" placeholder="1234 Main St" required>
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="validationCustom03">City</label>
+                        <div class="col-md-12 mb-3">
+                        <input type="text" value='' name="city" type="text" class="form-control" id="validationCustom03" placeholder="City" required>
+                            <div class="invalid-feedback">
+                                Please provide a valid city.
+                            </div>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                    <label for="validationCustom04">Province</label>
+                    <div class="col-md-12 mb-3">
+                        <input value='' name="province" type="text" class="form-control" id="validationCustom04" placeholder="Province" required>
+                        <div class="invalid-feedback">
+                            Please provide a valid state.
+                        </div>
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                    <label for="validationCustom05">Zip Code</label>
+                    <div class="col-md-12 mb-3">
+                        <input value='' name="zip_code" type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
+                        <div class="invalid-feedback">
+                            Please provide a valid zip.
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="form-row">
+                <div class="col-sm-4 mb-3">
+                    <label for="validationCustom01">First name</label>
+                    <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="" name="first_name" required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                 </div>
+                <div class="col-md-4 mb-3">
+                    <label for="validationCustom01">Middle name</label>
+                    <input type="text" class="form-control" id="validationCustom01" placeholder="Middle name" value="" name="middle_name" required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                 </div>
+                 <div class="col-md-4 mb-3">
+                    <label for="validationCustom02">Last name</label>
+                    <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="" name="last_name" required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <label for="inputAddress">Address</label>
+                <input type="text" name="street_address" class="form-control" id="inputAddress" placeholder="1234 Main St" required>
+            </div>
+            <div class="form-row">
+                <div class="col-md-6 mb-3">
+                    <label for="validationCustom03">City</label>
+                    <input name="city" type="text" name="city" class="form-control" id="validationCustom03" placeholder="City" required>
+                    <div class="invalid-feedback">
+                        Please provide a valid city.
+                    </div>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <label for="validationCustom04">State</label>
+                    <input name="province" type="text" class="form-control" id="validationCustom04" placeholder="Province" required>
+                    <div class="invalid-feedback">
+                        Please provide a valid state.
+                    </div>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <label for="validationCustom05">Zip</label>
+                    <input name="zip_code" type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
+                    <div class="invalid-feedback">
+                        Please provide a valid zip.
+                    </div>
+                </div>
+            </div> -->
+            <!-- <div class="form-group">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                    <label class="form-check-label" for="invalidCheck">
+                        Agree to terms and conditions
+                    </label>
+                    <div class="invalid-feedback">
+                        You must agree before submitting.
+                    </div>
+                </div>
+            </div> -->
+            <button class="btn btn-primary" type="submit">Submit form</button>
+        </form>
+
+        <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+            });
+        }, false);
+        })();
+        </script>
+    </div>
+    <br/>
+  </main>
+
